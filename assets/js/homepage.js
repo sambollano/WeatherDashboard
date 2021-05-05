ff16a1e97cc02804412527cd70921fe7298f58de
Samantha <Sambollano54@gmail.com>
2021-05-05 06:33:17 -0500

    API Key

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,7 +2,8 @@ var getUserRepos = function(user) {
   // format the github api url
   var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=Nixon&appid=9e398d752f41483a9829912733925aab" + user + "/repos";
 
-  const apiKey =9e398d752f41483a9829912733925aab
+  const apiKey = '9e398d752f41483a9829912733925aab';
+  const userFormEl = document
 
 
   fetch(apiUrl).then(function(response) {
